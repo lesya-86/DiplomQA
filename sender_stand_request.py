a7c91ad1d84942eab6cb3da54acf1833ad7c1639
Lesya-86 <lesyasokol86@gmail.com>
2024-04-21 22:13:58 +0700

    My commit

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -3,7 +3,7 @@ import configuration
 import data
 
 def post_new_order():
-    return requests.post(configuration.URL_SERVER + configuration.CREATE_NEW_ORDER,
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_NEW_ORDER,
                          json=data.order_body)
-def get_info_track(track_id):
-    return requests.get(configuration.URL_SERVER + configuration.GET_INFO_TRACK + str(track_id))
+def get_orders_track(track_id):
+    return requests.get(configuration.URL_SERVICE + configuration.GET_INFO_BY_TRACK + str(track_id))
